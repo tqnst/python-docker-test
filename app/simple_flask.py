7fc81218ae1a0523fb5a922b5a79292dfa09bab9
Ngoc To <ngoctq@mobilus.co.jp>
2018-03-15 14:56:34 +0900

    add redis to test

--- app/simple_flask.py
+++ app/simple_flask.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from redis import Redis
 application = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello World!</h1>"
+    count = redis.incr('hits')
+    return "<h1 style='color:red'>Hello World!</h1><br/>" + count
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
