bb93db38b0aeb421b474919a9796230bc71f7107
Ngoc To <ngoctq@mobilus.co.jp>
2018-03-15 15:45:37 +0900

    fix python code

--- app/simple_flask.py
+++ app/simple_flask.py
@@ -6,7 +6,7 @@ redis = Redis(host='redis', port=6379)
 @application.route("/")
 def hello():
     count = redis.incr('hits')
-    return "<h1 style='color:red'>Hello World!</h1><br/>" + count
+    return "<h1 style='color:red'>Hello World!</h1><br/>" + str(count)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
